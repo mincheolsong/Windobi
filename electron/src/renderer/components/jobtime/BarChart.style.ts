f0228ef12318c9bcd16c4147874c46bf865dd822
이우철 <lwc421@gmail.com>
2023-10-26 13:44:25 +0900

    feat: 데일리 차트 완성
    Former-commit-id: 08fbb9d8eb06417511b1120b1b3c572ee4afe82d

--- electron/src/renderer/components/jobtime/BarChart.style.ts
+++ electron/src/renderer/components/jobtime/BarChart.style.ts
@@ -16,15 +16,32 @@ const Li = styled.li`
   width: 100%;
   display: flex;
   flex-direction: row;
+  align-items: center;
   margin-bottom: 10px;
 `;
 
-const Bar = styled.div`
-  width: ${(props) => props.percentage}%;
-  height: 20px;
-  margin-left: 10px;
-  margin-right: 10px;
-  background-color: red;
+const Image = styled.img`
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
-export { Wrapper, Ul, Li, Bar };
+type TBar = {
+  percentage: number;
+  barcolor: string;
+};
+const Bar = styled.div<TBar>`
+  width: ${(props) => props.percentage}%;
+  height: 24px;
+  margin-left: 10px;
+  margin-right: 10px;
+  padding-left: 10px;
+  border-radius: 10px;
+
+  background-color: ${(props) => props.barcolor};
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+`;
+
+export { Wrapper, Ul, Li, Image, Bar };
